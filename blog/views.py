f22b4f54cb8da288de03751481903dd29e783a68
Shrinidhi SS <ss.shrinidhi.98@gmail.com>
2020-08-23 12:23:58 +0530

    Databases and Migrations

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author':'CoreyMS',
-        'title':'Blog Post 1',
-        'content':'First post content',
-        'date_posted':'January 28,2020'
-    },
-    {
-        'author':'Shri',
-        'title':'Blog Post 2',
-        'content':'Second post content',
-        'date_posted':'March 19,2020'
-    }
-]
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html',context)
 
